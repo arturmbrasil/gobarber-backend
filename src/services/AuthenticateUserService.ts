dffb26415e760a89e3a4294b674beda203aa1152
Artur Brasil <arturmbrasil@hotmail.com>
2020-04-16 21:36:40 -0300

    token jwt

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -31,8 +33,12 @@ class AuthenticateUserService {
       throw new Error('Incorrect Email/Password combination');
     }
 
-    // Usuário autenticado
-    return { user };
+    const token = sign({}, '381c2c47709bd3403e02d411e43e1d45', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return { user, token };
   }
 }
 
