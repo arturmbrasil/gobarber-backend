3e67895268f4429dad6b716674ed16dd64eabaf4
Artur Brasil <arturmbrasil@hotmail.com>
2020-04-17 21:07:05 -0300

    file upload

---
+++ src/database/migrations/1587166541338-AddAvatarFieldToUser.ts
@@ -0,0 +1,19 @@
+import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
+
+export default class AddAvatarFieldToUser1587166541338
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.addColumn(
+      'users',
+      new TableColumn({
+        name: 'avatar',
+        type: 'varchar',
+        isNullable: true,
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.dropColumn('users', 'avatar');
+  }
+}
