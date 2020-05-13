404d13defd4fcc1269aba1f78da36465821db2dd
Artur Brasil <arturmbrasil@hotmail.com>
2020-05-12 23:11:29 -0300

    list available days/hours

---
+++ src/modules/appointments/services/ListProviderDayAvailabilityService.ts
@@ -0,0 +1,62 @@
+import { inject, injectable } from 'tsyringe';
+import { getHours } from 'date-fns';
+
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+
+interface IRequest {
+  provider_id: string;
+  day: number;
+  month: number;
+  year: number;
+}
+
+type IResponse = Array<{
+  hour: number;
+  available: boolean;
+}>;
+
+@injectable()
+class ListProviderDayAvailabilityService {
+  constructor(
+    @inject('IAppointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository,
+  ) {}
+
+  public async execute({
+    provider_id,
+    month,
+    year,
+    day,
+  }: IRequest): Promise<IResponse> {
+    const appointments = await this.appointmentsRepository.findAllInDayFromProvider(
+      {
+        provider_id,
+        month,
+        year,
+        day,
+      },
+    );
+
+    const hourStart = 8;
+
+    const eachHourArray = Array.from(
+      { length: 10 },
+      (value, index) => index + hourStart,
+    );
+
+    const availability = eachHourArray.map(hour => {
+      const hasAppointmentInHours = appointments.find(
+        appointment => getHours(appointment.date) === hour,
+      );
+
+      return {
+        hour,
+        available: !hasAppointmentInHours,
+      };
+    });
+
+    return availability;
+  }
+}
+
+export default ListProviderDayAvailabilityService;
